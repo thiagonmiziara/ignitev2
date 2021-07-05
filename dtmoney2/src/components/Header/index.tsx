217bf1ffc0fdff4c5b5896edd37b7f11e35486a1
Thiago Miziara <thiagonmiziara@gmail.com>
2021-07-05 19:35:09 -0300

    add react-modal

--- dtmoney2/src/components/Header/index.tsx
+++ dtmoney2/src/components/Header/index.tsx
@@ -1,18 +1,20 @@
-import logoImg from "../../assets/logo.svg";
+import logoImg from '../../assets/logo.svg'
 
-import { Container, Content } from "./style";
+import { Container, Content } from './style'
 
-export const Header = () => {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export const Header = ({onOpenNewTransactionModal}:HeaderProps) => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
       </Content>
     </Container>
   )
 }
-
-
