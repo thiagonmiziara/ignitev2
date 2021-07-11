24daf1b98292613edc67cd68ca32fd248ac562df
Thiago Miziara <thiagonmiziara@gmail.com>
2021-07-11 19:24:15 -0300

    armazenando os values dos inputs do form

--- dtmoney2/src/components/NewTransactionModal/index.tsx
+++ dtmoney2/src/components/NewTransactionModal/index.tsx
@@ -1,66 +1,88 @@
-import { useState } from 'react'
-import Modal from 'react-modal'
-import closeImg from '../../assets/close.svg'
-import incomeImg from '../../assets/income.svg'
-import outcomeImg from '../../assets/outcome.svg'
+import { useState, FormEvent } from "react";
+import Modal from "react-modal";
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
-import { useModalNewTransactions } from '../hooks/useModalNewTransactions'
+import { useModalNewTransactions } from "../hooks/useModalNewTransactions";
 
-import { Container, RadioBox, TransactionTypeContainer } from './styles'
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 export const NewTransactionModal = () => {
-  const [type, setType] = useState('deposit')
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("deposit");
 
-  const { handleCloseNewTransactionModal, isNewTransactionModalOpen } = useModalNewTransactions()
+  const { handleCloseNewTransactionModal, isNewTransactionModalOpen } =
+    useModalNewTransactions();
+
+  const handleCreateNewTransaction = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(title, value, category, type);
+  };
 
   return (
     <Modal
       isOpen={isNewTransactionModalOpen}
       onRequestClose={handleCloseNewTransactionModal}
-      overlayClassName="react-modal-overlay"
-      className="react-modal-content"
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
     >
-     <button
-        type="button"
+      <button
+        type='button'
         onClick={handleCloseNewTransactionModal}
-        className="react-modal-close"
+        className='react-modal-close'
       >
-        <img src={closeImg} alt="Fechar Modal" />
+        <img src={closeImg} alt='Fechar Modal' />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder="Titulo" />
+        <input
+          placeholder='Titulo'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-        <input type="number" placeholder="Valor" />
+        <input
+          type='number'
+          placeholder='Valor'
+          value={value}
+          onChange={(e) => setValue(Number(e.target.value))}
+        />
 
         <TransactionTypeContainer>
-         <RadioBox
-            type="button"
-            onClick={()=>setType('deposit')}
-            isActive={type === 'deposit'}
-            activeColor="green"
+          <RadioBox
+            type='button'
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+            activeColor='green'
           >
-            <img src={incomeImg} alt="Entrada" />
+            <img src={incomeImg} alt='Entrada' />
             <span>Entrada</span>
           </RadioBox>
 
           <RadioBox
-           type="button"
-           onClick={() => setType('withdraw')} 
-           isActive={type === 'withdraw'} 
-           activeColor="red"
+            type='button'
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+            activeColor='red'
           >
-            <img src={outcomeImg} alt="Saída" />
+            <img src={outcomeImg} alt='Saída' />
             <span>Saída</span>
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input
+          placeholder='Categoria'
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
 
-        <button type="submit">Cadastrar</button>
+        <button type='submit'>Cadastrar</button>
       </Container>
     </Modal>
-  )
-}
+  );
+};
