2ef31714c3626acaeedec62268055b09087a6b8a
Thiago Miziara <thiagonmiziara@gmail.com>
2021-08-09 17:53:49 -0300

    add page home

--- ignews2/ingnews2/ignews/src/pages/index.tsx
+++ ignews2/ingnews2/ignews/src/pages/index.tsx
@@ -1,15 +1,30 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import girlCodingImg from "/public/images/avatar.svg";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <div>
-        <h1>Teste</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image src={girlCodingImg} alt='Girl coding' />
+      </main>
     </>
   );
 }
