9c0ba7428f69146308f06dfe965fc7f01ae9270f
Thiago Miziara <thiagonmiziara@gmail.com>
2021-08-12 19:09:52 -0300

    add stripe api request in SSR

--- ignews2/ingnews2/ignews/src/pages/index.tsx
+++ ignews2/ingnews2/ignews/src/pages/index.tsx
@@ -1,12 +1,26 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-import girlCodingImg from "/public/images/avatar.svg";
+import { stripe } from "../service/stripe";
+import { formatPrice } from "../service/formatPrice";
 
-import styles from "./home.module.scss";
+import girlCodingImg from "/public/images/avatar.svg";
 import { SubscribeButton } from "../components/SubscribeButton";
 
-export default function Home() {
+import styles from "./home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+    descriptionProduct: {
+      description: string;
+    };
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -21,12 +35,33 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
+            {/* <p>{product.descriptionProduct.description}</p> */}
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <Image src={girlCodingImg} alt='Girl coding' />
       </main>
     </>
   );
 }
+
+//function SSR so funciona em pagina e se quiser acessar alguma informação em um component
+// tem que passar via props para o component
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JNlZOFh90upBGI50srgSBhg", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: formatPrice(price.unit_amount / 100),
+    descriptionProduct: price.product,
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
