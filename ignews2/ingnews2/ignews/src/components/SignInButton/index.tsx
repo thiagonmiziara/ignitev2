de76c389be76f110d2b7481d948c39e461f9bd3b
Thiago Miziara <thiagonmiziara@gmail.com>
2021-08-17 15:26:22 -0300

    add api/routes and next-auth github and google providers

--- ignews2/ingnews2/ignews/src/components/SignInButton/index.tsx
+++ ignews2/ingnews2/ignews/src/components/SignInButton/index.tsx
@@ -1,21 +1,44 @@
 import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { RiLoginCircleFill } from "react-icons/ri";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession, Provider } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button type='button' className={styles.signInButton}>
-      <FaGithub color='#04d361' />
-      Thiago Miziara
+  return session ? (
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <RiLoginCircleFill color='#04d361' />
+
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button type='button' className={styles.signInButton}>
-      <FaGithub color='#eba417' />
-      Sign In with GitHub
-    </button>
+    <>
+      <button
+        type='button'
+        className={styles.signInButton}
+        onClick={() => signIn("google")}
+      >
+        <FcGoogle color='#eba417' />
+        Sign In with Google
+      </button>
+
+      <button
+        type='button'
+        className={styles.signInButton}
+        onClick={() => signIn("github")}
+      >
+        <FaGithub color='#eba417' />
+        Sign In with GitHub
+      </button>
+    </>
   );
 }
