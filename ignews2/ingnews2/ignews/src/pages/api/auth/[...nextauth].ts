7c395d9230c90228807dc3dd0a9219e46b2709d1
Thiago Miziara <thiagonmiziara@gmail.com>
2021-08-21 18:59:00 -0300

    add cominication api fauna

--- ignews2/ingnews2/ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews2/ingnews2/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -13,4 +16,16 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
