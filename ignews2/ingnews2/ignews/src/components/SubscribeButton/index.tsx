7afe2eab79e241bafd25277fce64cda8d56c54c2
Thiago Miziara <thiagonmiziara@gmail.com>
2021-08-23 19:55:37 -0300

    add api route subscribe.ts

--- ignews2/ingnews2/ignews/src/components/SubscribeButton/index.tsx
+++ ignews2/ingnews2/ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,8 +6,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      //testar para ver se funciona
+      signIn("github" || "google");
+      return;
+    }
+  }
+
   return (
-    <button type='button' className={styles.subscribeButton}>
+    <button
+      type='button'
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
