cafe4e73f85a5763b06588f48f2df9eaf1d33dec
Thiago Miziara <thiagonmiziara@gmail.com>
2021-09-25 14:31:38 -0300

    retomando estudos ignite

--- ignews2/ingnews2/ignews/src/components/SubscribeButton/index.tsx
+++ ignews2/ingnews2/ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -8,11 +10,20 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       //testar para ver se funciona
       signIn("github" || "google");
       return;
+    }
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
     }
   }
 
